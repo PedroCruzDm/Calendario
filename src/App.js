2dd9b6c25b8545a365bfcab4a721179c6e99a1bc
O Famosoo Pedro <joaopedropaulohenrique@gmail.com>
2025-05-02 14:43:56 -0300

    reconstruindo projeto (commit deu problema)

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import React from 'react';
+import Calendario from './components/Calendario/calendario.jsx';
+
+function App() {
+  return (
+    <div className='App Calendario'>
+      <Calendario />
+    </div>
+  );
+}
+
+export default App;
